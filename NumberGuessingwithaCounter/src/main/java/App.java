19c3f2463e70c3aa7780090adb51320235b87bfd
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-22 20:47:09 -0400

    Add some logic

--- NumberGuessingwithaCounter/src/main/java/App.java
+++ NumberGuessingwithaCounter/src/main/java/App.java
@@ -2,5 +2,19 @@ public class App {
 
     public static void main(String[] args) {
 
+        logic();
+
     }
+
+    static void logic(){
+
+        System.out.println("I have chosen a number between 1 and 10.  Try to guess it.\n");
+        System.out.print("Your guess: ");
+
+
+
+
+
+    }
+
 }
